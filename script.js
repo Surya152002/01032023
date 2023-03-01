93bacfa8477a21ef2675115b13b5b8de9743fc29
Surya152002 <108388181+Surya152002@users.noreply.github.com>
2023-03-01 12:20:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,74 +77,74 @@ function deviceOrientationListener(event) {
      ctx.clearRect(0,0, c.width, c.height);
      ctx.fillStyle="#00000";
      ctx.font="20px Verdana";
-     ctx.fillText("Alpha:" + Math.round(event.alpha) ,10,40);
+     //ctx.fillText("Alpha:" + Math.round(event.alpha) ,10,40);
      ctx.fillText("Beta:" + Math.round(event.beta),10,80);
-     ctx.fillText("Gamma:" + Math.round(event.gamma),10,120);
+     //ctx.fillText("Gamma:" + Math.round(event.gamma),10,120);
     
-    if(Math.round(event.beta)<93 && (event.beta)>85    )
-    {
+//     if(Math.round(event.beta)<93 && (event.beta)>85    )
+//     {
         
-                    ctx.fillText("Orientation of Seating:" + "Proper Seating Mode",10,240);
+//                     ctx.fillText("Orientation of Seating:" + "Proper Seating Mode",10,240);
                         
                
-    }
-    if(Math.round(event.alpha)>0)
-    {
-        if(Math.round(event.beta)>0){
-            if(Math.round(event.gamma)>0){
-                ctx.fillText("User Started using Application",10,200); 
+//     }
+//     if(Math.round(event.alpha)>0)
+//     {
+//         if(Math.round(event.beta)>0){
+//             if(Math.round(event.gamma)>0){
+//                 ctx.fillText("User Started using Application",10,200); 
                 
 
                     
                 
-            }
-        }
-    }
+//             }
+//         }
+//     }
     
-        if(Math.round(event.beta)<80){
+//         if(Math.round(event.beta)<80){
             
-                ctx.fillText("Orientation of Seating:" + "Not Proper Seating Mode",10,240);
+//                 ctx.fillText("Orientation of Seating:" + "Not Proper Seating Mode",10,240);
                     
             
-        }
+//         }
     
-        if(Math.round(event.gamma )<55 &&   Math.round(event.beta)<27   )
-            {
+//         if(Math.round(event.gamma )<55 &&   Math.round(event.beta)<27   )
+//             {
                 
                     
                         
-                            ctx.fillText("Thanks for using Smart Chair",10,280);
+//                             ctx.fillText("Thanks for using Smart Chair",10,280);
                                 
                         
                     
                 
-            }
-        else{
-               if(Math.round(event.alpha)==0)
-                    {
-                        if(Math.round(event.beta)==0){
-                            if(Math.round(event.gamma)==0){
-                                ctx.fillText("Thanks for using Smart Chair",10,280);
+//             }
+//         else{
+//                if(Math.round(event.alpha)==0)
+//                     {
+//                         if(Math.round(event.beta)==0){
+//                             if(Math.round(event.gamma)==0){
+//                                 ctx.fillText("Thanks for using Smart Chair",10,280);
                                     
-                            }
-                        }
-                    } 
-        }
+//                             }
+//                         }
+//                     } 
+//         }
 
-     var img = document.getElementById('cd');
-     var GammaVal = round(event.gamma);
-     var ForceInRange = Math.floor(GammaVal);
-     if (GammaVal <= 36) {   img.src = images[36]; } // Constrain first image
-      else if (GammaVal >= 1)  {  img.src = images[lastImageNumber]; } //Constrain last image
-        else img.src = images[ForceInRange];
-ctx.fillText("Using Image:" + img.src ,10,160);
-}
+//      var img = document.getElementById('cd');
+//      var GammaVal = round(event.gamma);
+//      var ForceInRange = Math.floor(GammaVal);
+//      if (GammaVal <= 36) {   img.src = images[36]; } // Constrain first image
+//       else if (GammaVal >= 1)  {  img.src = images[lastImageNumber]; } //Constrain last image
+//         else img.src = images[ForceInRange];
+// ctx.fillText("Using Image:" + img.src ,10,160);
+// }
 
 
-// Orientation Check
-var supportsOrientation = "No";
-if (window.DeviceOrientationEvent) {
-   window.addEventListener("deviceorientation", deviceOrientationListener);
-   var supportsOrientation = "Yes";
-   } else {   document.getElementById("dmEvent").innerHTML = "deviceorientation Not Supported!"}
-//END Check to see if browser supports motion
+// // Orientation Check
+// var supportsOrientation = "No";
+// if (window.DeviceOrientationEvent) {
+//    window.addEventListener("deviceorientation", deviceOrientationListener);
+//    var supportsOrientation = "Yes";
+//    } else {   document.getElementById("dmEvent").innerHTML = "deviceorientation Not Supported!"}
+// //END Check to see if browser supports motion
